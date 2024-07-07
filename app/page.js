45b5bd3b8ddbc1f512ed60d0749ca7884691890f
Darshan Prajapati <codewithdp2073@gmail.com>
2024-07-07 17:04:33 +0530

    Add Task completed

--- app/page.js
+++ app/page.js
@@ -4,13 +4,22 @@ import React, { useState } from 'react'
 const page = () => {
   const [title, settitle] = useState("")
   const [desc, setdesc] = useState("")
+  const [mainTask, setmainTask] = useState([])
   const submitHandler = (e) => {
     e.preventDefault()
-    console.log(title)
-    console.log(desc)
-    console.log("Submit kar diya")
+    setmainTask([...mainTask, { title, desc }])
     settitle("")
     setdesc("")
+    // console.log(mainTask)
+  }
+  let randerTask = <h2>No Task Availabel</h2>
+  if (mainTask.length > 0) {
+    randerTask = mainTask.map((t, i) => {
+      return <li><div className='flex justify-between mb-5'>
+        <h5 className='text-2xl font-semibold'>{t.title}</h5>
+        <h6 className='text-xl font-semibold'>{t.desc}</h6>
+      </div></li>
+    })
   }
   return (
     <>
@@ -24,6 +33,10 @@ const page = () => {
         }} />
         <button className='bg-black text-white px-4 py-3 font-bold rounded text-2xl m-5'>Add Task</button>
       </form>
+      <hr />
+      <div className='p-8 bg-slate-200'>
+        <ul>{randerTask}</ul>
+      </div>
     </>
   )
 }
