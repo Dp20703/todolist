72644a66f7383c0bad8afdcdf886834363dede79
Darshan Prajapati <codewithdp2073@gmail.com>
2024-07-19 12:17:28 +0530

    update functionality added

--- app/Addtask.js
+++ app/Addtask.js
@@ -1,11 +1,16 @@
 'use client'
 
+import { Courier_Prime, Fascinate } from 'next/font/google';
 import React, { useState } from 'react'
 
 const Addtask = () => {
-    const [title, settitle] = useState("")
-    const [desc, setdesc] = useState("")
+    const [isEditing, setisEditing] = useState(false)
+    let [title, settitle] = useState("")
+    let [desc, setdesc] = useState("")
     const [mainTask, setmainTask] = useState([])
+    const [eTitle, seteTitle] = useState("")
+    const [eDesc, seteDesc] = useState("")
+
     const submitHandler = (e) => {
         e.preventDefault()
         setmainTask([...mainTask, { title, desc }])
@@ -22,13 +27,34 @@ const Addtask = () => {
         setmainTask(copyTask)
 
     }
-    // const updateHandler = (e,i) => {
-    //   e.preventDefault()
-    //   setmainTask([...mainTask, { title, desc }])
-    //   settitle("")
-    //   setdesc("")
-    //   // console.log(mainTask)
-    // }
+
+    function handleEditClick(i, etitle, edesc) {
+        let UpdateTitle = etitle
+        let updateDesc = edesc
+        seteTitle(UpdateTitle)
+        seteDesc(updateDesc)
+        console.log(UpdateTitle)
+        console.log(updateDesc)
+        let copyTask = [...mainTask]
+        copyTask.splice(i, 1)
+        setmainTask(copyTask)
+        setisEditing(true)
+    }
+
+    function handleEditFormSubmit(e) {
+        e.preventDefault();
+        title = eTitle
+        console.log(title)
+        desc = eDesc
+        console.log(desc)
+        setmainTask([...mainTask, { title, desc }])
+        seteTitle("")
+        seteDesc("")
+        console.log(mainTask)
+        setisEditing(false);
+    }
+
+
     let randerTask = <h2>No Task Availabel</h2>
     if (mainTask.length > 0) {
         randerTask = mainTask.map((t, i) => {
@@ -41,29 +67,54 @@ const Addtask = () => {
                     deleteHandler(i)
                 }}
                     className='bg-red-600 text-white rounded px-2 py-2'>Delete</button>
-                {/* <button onClick={() => {
-          updateHandler(i)
-          }}className='bg-green-600 text-white rounded px-2 py-2'>Update</button> */}
+                <button onClick={() => {
+                    handleEditClick(i, t.title, t.desc)
+                }} className='bg-blue-600 text-white rounded px-2 py-2'>Update</button>
             </li>
         })
     }
     return (
-        <>
-            <h1 className='bg-black text-white font-bold text-center text-5xl p-5 w-screen'>Dp's To-do List</h1>
-            <form onSubmit={submitHandler} >
-                <input type="text" className='text-2xl px-4 py-2 m-8 border-zinc-700 border-4 w-[330px]  md:w-[400px]  lg:w-[400px]' required placeholder='Enter Title here' value={title} onChange={(e) => {
-                    settitle(e.target.value)
-                }} />
-                <input type="text" className='text-2xl px-4 py-2 m-8 border-zinc-700 border-4 w-[330px] md:w-[400px] lg:w-[400px]' placeholder='Enter Description here' required value={desc} onChange={(e) => {
-                    setdesc(e.target.value)
-                }} />
-                <button className='bg-black text-white px-4 py-3 font-bold rounded text-2xl m-5'>Add Task</button>
-            </form>
-            <hr />
-            <div className='p-8 bg-slate-200 '>
-                <ul>{randerTask}</ul>
-            </div>
-        </>
+        <div className='parent'>
+            {isEditing ?
+                <form onSubmit={handleEditFormSubmit}>
+
+                    <h1 className='bg-black text-white font-bold text-center text-5xl p-5 w-screen'>Edit To-do List</h1>
+                    <div className='flex justify-around items-center mb-5'>
+                        <h2 className='text-center  bg-zinc-800 text-white px-4 ml-4 rounded py-4 font-bold w-1/4'>Edit Title:</h2>
+                        <input type="text" name="editTodo" id="editTodo" value={eTitle} onChange={(e) => {
+                            seteTitle(e.target.value)
+                        }} className='text-2xl px-4 py-2 m-8 border-zinc-700 border-4 w-[330px]  md:w-[400px]  lg:w-[400px]' required placeholder='Edit Title here' />
+                    </div>
+                    <div className='flex justify-around items-center mb-5'>
+                        <h2 className='text-center bg-zinc-800 text-white px-4 ml-4 rounded py-4 font-bold w-1/4'>Edit Description:</h2>
+                        <input type="text" name="editTodo" id="editTodo" value={eDesc} onChange={(e) => {
+                            seteDesc(e.target.value)
+                        }} className='text-2xl px-4 py-2 m-8 border-zinc-700 border-4 w-[330px] md:w-[400px] lg:w-[400px]' placeholder='Edit Description here' required />
+
+                    </div>
+                    <div className='flex justify-around items-center  '>
+                        <button type="submit" className='text-white bg-blue-600 py-2 px-2 rounded'>Update</button>
+                        <button className='text-white bg-red-600 py-2 px-2 rounded' type='submit' onClick={() => setisEditing(false)}>Cancel</button>
+                    </div>
+                </form>
+                :
+                (<>
+                    <h1 className='bg-black text-white font-bold text-center text-5xl p-5 w-screen'>Dp's To-do List</h1>
+                    <form onSubmit={submitHandler} >
+                        <input type="text" className='text-2xl px-4 py-2 m-8 border-zinc-700 border-4 w-[330px]  md:w-[400px]  lg:w-[400px]' required placeholder='Enter Title here' value={title} onChange={(e) => {
+                            settitle(e.target.value)
+                        }} />
+                        <input type="text" className='text-2xl px-4 py-2 m-8 border-zinc-700 border-4 w-[330px] md:w-[400px] lg:w-[400px]' placeholder='Enter Description here' required value={desc} onChange={(e) => {
+                            setdesc(e.target.value)
+                        }} />
+                        <button className='bg-black text-white px-4 py-3 font-bold rounded text-2xl m-5'>Add Task</button>
+                    </form>
+                    <hr />
+                    <div className='p-8 bg-slate-200 '>
+                        <ul>{randerTask}</ul>
+                    </div>
+
+                </>)}</div>
     )
 }
 
